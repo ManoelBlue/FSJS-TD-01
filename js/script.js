a1798c61a778e12dd94b849567fa41e6eac70112
Manoel Blue <manoelbluee@gmail.com>
2020-12-24 07:37:31 +0100

    Create printQuote function

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
  * `quotes` array 
 ***/
@@ -56,8 +52,23 @@ function getRandomQuote() {
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+    let quote = getRandomQuote();
+    let html = `
+        <p class="quote">QUOTE: ${quote.quote}</p>
+        <p class="source">SOURCE: ${quote.source}
+    `;
 
+    if(quote.citation) {
+        html += `<span class="citation">CITATION: ${quote.citation}</span>`;
+    };
+    if(quote.year) {
+        html += `<span class="year">YEAR: ${quote.year}</span>`;
+    };
+    html += `</p>`;
 
+    document.getElementById('quote-box').innerHTML = html;
+}
 
 /***
  * click event listener for the print quote button
